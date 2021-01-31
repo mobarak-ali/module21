5f0ce51f46d5dbc4b4d7a5010df7d3d1ecf59757
Mobarak Ali <mobarak.ali@outlook.com>
2021-01-31 14:23:55 +0600

    Code for DatePicke is updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Handlign TicketBooking 
+// Handlign Ticket Booking 
 function handleTicketBooking(ticketClass, addTicket) {
 
     const ticketInput = document.getElementById(ticketClass + '-class');
@@ -113,3 +113,36 @@ function bookNow() {
     }
 
 }
+
+/// Working with Date Value
+function filterDate(e) {
+    const getId = e.target.getAttribute('id');
+    const setDate = e.target.value;
+    const departureDate = document.getElementById("departure");
+    const returnDate = document.getElementById("return");
+
+    if (getId == 'departure') {
+        returnDate.setAttribute("min", setDate);
+    } else {
+        departureDate.setAttribute("max", setDate);
+    }
+}
+
+
+// This code executes on Load//
+
+// You can not select a date before Today!
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0 so need to add 1 to make it 1!
+var yyyy = today.getFullYear();
+if (dd < 10) {
+    dd = '0' + dd
+}
+if (mm < 10) {
+    mm = '0' + mm
+}
+
+today = yyyy + '-' + mm + '-' + dd;
+document.getElementById("departure").setAttribute("min", today);
+document.getElementById("return").setAttribute("min", today);
